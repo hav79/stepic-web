b54234ca3cb5530ffe116cb2775be2927f6e6503
hav79 <abambr@gmail.com>
2016-03-05 00:19:16 +0300

    gunicorn2

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
