7da5caaf1a5155dd88eb948b928d96cc9414492b
hav79 <abambr@gmail.com>
2016-03-13 16:01:22 +0300

    django12

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
+sudo rm /etc/gunicorn.d/*
 sudo ln -s /home/box/web/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
