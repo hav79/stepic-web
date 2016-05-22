c45af285eb17a3530976b0af8e0cf7caf6c4f5b1
hav79 <abambr@gmail.com>
2016-05-22 08:35:56 +0300

    django13

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns(
+urlpatterns = [
     
     url(r'^$', 'qa.views.test'),
     url(r'^login/', 'qa.views.test'),
@@ -11,4 +11,4 @@ urlpatterns = patterns(
     url(r'^ask/', 'qa.views.test'),
     url(r'^popular/', 'qa.views.test'),
     url(r'^new/', 'qa.views.test')
-)
+]
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Question(models.Model):
+    title = models.CharField()
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User)
+
+    def __unicode__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.OneToOneField(Question)
+    author = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.text
